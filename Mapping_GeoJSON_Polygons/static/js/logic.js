6d2adc7e6dbefed649a318306e4bf9b404924374
Quinton Wandzura <quinton.wandzura2@ucalgary.ca>
2020-07-18 19:37:53 -0600

    Uploading

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -9,7 +9,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
@@ -17,38 +17,43 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps. 
 let baseMaps = {
-	Street: streets,
-	Dark: dark
+	"Street": streets,
+	"Satellite Streets": satelliteStreets
 };
 
 // Create the map object with center and zoom level.
 let map = L.map('mapid', {
-	center: [30, 30],
-	zoom: 2,
+	center: [43.7, -79.3],
+	zoom: 10,
 	layers: [streets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-streets.addTo(map);
+let myStle = {
+	color : "blue",
+	weight: 1,
+	fill: "yellow"
+}
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/quintonw1/Mapping_Earthquakes/master/majorAirports.json";
+let torontoHoods = "https://raw.githubusercontent.com/quintonw1/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
 function createMarkers (data) {
 	console.log(data);
 	data.forEach(function(entry) {
-		L.geoJSON(entry,{
+		L.geoJSON(entry, {
 			onEachFeature: function(feature, layer) {
-				layer.bindPopup("<h2>Airport code: " + layer.feature.properties.faa + "</h2> <hr> <h3>Airport name: "+ layer.feature.properties.name + "</h3>")
+				layer.bindPopup("<h2>Neighborhood: " + layer.feature.properties.AREA_NAME)
 			}
 			}).addTo(map)
+		
 	});
 };
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
 	console.log(data);
 	createMarkers(data.features);
 });
